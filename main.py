ea250bdf28d2cd6157ae38109a7aa0766276535e
ntfwc <ntfwc@yahoo.com>
2015-04-17 22:54:53 -0500

    Updated copyright years.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-##    Copyright (C) 2014 ntfwc
+##    Copyright (C) 2014-2015 ntfwc
 ##
 ##    This program is free software: you can redistribute it and/or modify
 ##    it under the terms of the GNU General Public License as published by
