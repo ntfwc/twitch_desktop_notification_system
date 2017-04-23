26f40546368bac9c84abfa6036fd75131ee1083b
ntfwc <ntfwc@yahoo.com>
2017-04-22 23:27:53 -0500

    Added some more methods to the storage system for user IDs

--- lib/UserIdStorageSystem.py
+++ lib/UserIdStorageSystem.py
@@ -5,9 +5,21 @@ DBM_FILE = "cache/clientIds.dbm"
 class UserIdStorageSystem(object):
     def __init__(self):
         self.dbm = anydbm.open(DBM_FILE, "c")
-
+    
+    def hasUserId(self, userName):
+        return userName in self.dbm
+    
     def getUserId(self, userName):
         return self.dbm.get(userName)
-    
+
     def setUserId(self, userName, userId):
         self.dbm[userName] = userId
+
+    def createMapCopy(self):
+        mapCopy = {}
+        for userName, clientId in self.dbm.items():
+            mapCopy[userName] = clientId
+        return mapCopy
+
+    def close(self):
+        self.dbm.close()
